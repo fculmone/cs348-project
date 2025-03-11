75e5497b587cfda80f6673a306415c2600b904a9
Francesco Culmone <fculmone5@gmail.com>
2025-03-11 19:30:33 -0400

    renamed old schema since I realized favourite cities was implemented

--- sql-used-in-project/city_schema.sql
+++ sql-used-in-project/city_schema.sql
@@ -5,22 +5,8 @@ USE cs348_project_db;
 -- ALSO probably want to make one schema for all tables so we can just paste that in
 --  and create database with some pre-filled data
 
-
--- the user table. Added a profile picture and removed email just so that 
---   there are less things to manage
-DROP TABLE IF EXISTS user;
-CREATE TABLE user (
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL, 
-    pfp VARCHAR(255) NOT NULL,
-    PRIMARY KEY (username)
-);
-
-INSERT INTO user (username, password, pfp)
-VALUES
-('bart', 'password1', 'pfp0.jpg');
-
-
+-- The only one currently in use is favourite_cities. For now, the main city table is
+--  top-100-cities-schema.sql
 
 -- Create city table
 DROP TABLE IF EXISTS city;
