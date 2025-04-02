c6ae1882c18cc8e06bbf5407de68c3ab9599e57d
happy1203 <happyvpatel12@gmail.com>
2025-04-02 13:26:17 -0400

    "updated monthly_favourite updating table"

--- cs348-nextjs/app/api/monthly_fav/route.js
+++ cs348-nextjs/app/api/monthly_fav/route.js
@@ -12,23 +12,48 @@ export async function GET(request) {
         `;
         const [rows] = await db.query(fetchSql);
 
-        const updateSql = `
+        const insertSQL = `
+        INSERT INTO most_favourited (city_id, city_name, country, total_favourites, last_updated)
+        SELECT
+            t.ranking AS city_id,   
+            t.city AS city_name,
+            t.country AS country,
+            COUNT(fc.username) AS total_favourites,
+            NOW() AS last_updated
+        FROM
+            favourite_cities fc
+        JOIN
+            top100_cities t ON fc.city_id = t.ranking 
+        GROUP BY
+            t.ranking, t.city, t.country
+        HAVING city_id NOT IN (SELECT city_id FROM most_favourited);
+
+        `;
+
+        await db.query(insertSQL);
+
+        const updateSQL = `
         UPDATE most_favourited mf
         JOIN (
-            SELECT 
-                t.ranking AS city_id, 
-                COUNT(fc.username) AS total_favourites,
-                NOW() AS last_updated
-            FROM top100_cities t
-            LEFT JOIN favourite_cities fc ON fc.city_id = t.ranking
-            GROUP BY t.ranking
+            SELECT
+                t.ranking AS city_id,   
+                COUNT(fc.username) AS total_favourites
+            FROM
+                favourite_cities fc
+            JOIN
+                top100_cities t ON fc.city_id = t.ranking 
+            GROUP BY
+                t.ranking
         ) temp ON mf.city_id = temp.city_id
-        SET mf.total_favourites = temp.total_favourites, mf.last_updated = temp.last_updated;
-        `;
-        await db.query(updateSql);
+        SET
+            mf.total_favourites = temp.total_favourites,
+            mf.last_updated = NOW();
+        `
+        await db.query(updateSQL);
 
         return NextResponse.json(rows);
     } catch (error) {
+        console.error("Error updating most_favourited:", error);
         return NextResponse.json({ error: "failed" });
     }
 }
